4e6a736f0c96bd1867d8c9a4ae67b1ce725c77e7
Parker <pkroberg@gmail.com>
2024-01-19 21:14:54 -0700

    Header css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,24 @@ export default {
   purge: ["*html"],
   content: [],
   theme: {
+    colors: {
+      greyscale1: "#F8F9FA",
+      greyscale2: "#E9ECEF",
+      greyscale3: "#DEE2E6",
+      greyscale4: "#CED4DA",
+      greyscale5: "#ADB5BD",
+      greyscale6: "#6C757D",
+      greyscale7: "#495057",
+      greyscale8: "#343A40",
+      greyscale9: "#212529",
+    },
     extend: {
       fontFamily: {
         sans: ["Inconsolata", "monospace", "Open Sans", "sans-serif"],
       },
+    },
+    height: {
+      53: "53px",
     },
   },
   plugins: [],
