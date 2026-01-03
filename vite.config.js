7d3d36780e20855960d927712a16e74f98e91001
pkroberg <pkroberg@gmail.com>
2026-01-02 17:51:20 -0700

    ensures vite config uses multi-page build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        gallery: "gallery.html",
+        auto_marine: "auto_marine.html",
+        commercial_healthcare: "commercial_healthcare.html",
+        furniture: "furniture.html",
+        testimonials: "testimonials.html",
+        contact: "contact.html"
+      }
+    }
+  }
 });
